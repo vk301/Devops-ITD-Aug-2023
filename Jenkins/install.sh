bde78f0288873d2dab86ecac258a2f4e2e0c25d9
jaintpharsha <40075520+jaintpharsha@users.noreply.github.com>
2023-10-27 09:36:16 +0530

    Update install.sh

--- Jenkins/install.sh
+++ Jenkins/install.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
-echo "STEP_1 Update apt"
+echo -e "\n---------------------------- STEP_1 Update apt ----------------------------\n"
+	sleep 4
 	sudo apt update 
 
-echo "STEP_2: Install Java ...."
+echo -e "\n---------------------------- STEP_2: Install Java .... ----------------------------\n"
+	sleep 4
 	sudo apt install fontconfig openjdk-17-jre
 
-echo "STEP_3 Add Jenkins to apt list" 
+echo -e "\n---------------------------- STEP_3 Add Jenkins to apt list ----------------------------\n" 
+	sleep 4
 	sudo wget -O /usr/share/keyrings/jenkins-keyring.asc https://pkg.jenkins.io/debian/jenkins.io-2023.key
 	echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
 	sudo apt update
 
-echo "STEP_4 Install Jenkins"
-	sudo apt install jenkins
+echo -e "\n---------------------------- STEP_4 Install Jenkins ----------------------------\n"
+	sleep 4
+ 	sudo apt install jenkins
 
-echo "STEP_5 Check Jenkins service status"
+echo -e "\n ---------------------------- STEP_5 Check Jenkins service status ----------------------------\n"
 	sudo systemctl status jenkins 
 
-echo "STEP_6: Open default port of Jenkins 8080 & secondary port 50000 in ec2 SG"
+echo -e "\n ---------------------------- STEP_6: Open default port of Jenkins 8080 & secondary port 50000 in ec2 SG ----------------------------"
 
-echo "STEP_7 Get Initial Admin Password"
+echo -e "\n---------------------------- STEP_7 Get Initial Admin Password ----------------------------"
 	sudo cat /var/lib/jenkins/secrets/initialAdminPassword
 
