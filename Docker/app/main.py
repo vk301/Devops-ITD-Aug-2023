3ec6a3657d0c78d1f2a7d88f873a1b10f15e5291
Harsha Jain <jaintpharsha@gmail.com>
2023-11-11 09:35:17 +0530

    update 11-November-23 09:35AM

--- Docker/app/main.py
+++ Docker/app/main.py
@@ -1,15 +1,26 @@
 from flask import Flask
+import socket
+
 app = Flask(__name__)
 
 @app.route('/')
-# ‘/’ URL is bound with hello_world() function.
 def say_hello():
-    return 'HELLO this is from Docker container'
+	return 'HELLO this is from Docker container'
 
 @app.route('/name')
-# ‘/’ URL is bound with hello_world() function.
 def return_name():
-    return 'My name is HARSHA'
+	return 'My name is HARSHA'
+
+@app.route('/hostname')
+def return_hostname():
+	hostname = socket.gethostname()
+	return hostname
+
+@app.route('/ip')
+def return_ip():
+	hostname = socket.gethostname()
+	get_ip = socket.gethostbyname(hostname)
+	return get_ip
 
 if __name__ == '__main__':
 	app.run(host="0.0.0.0", port="8080")
